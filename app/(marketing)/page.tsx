e901277f876519d044a7e2a64810504abe14f1bd
herohasan0 <kahraman.h@outlook.com>
2024-03-08 23:21:04 +0300

    add fonts

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,34 @@
 import {Medal} from "lucide-react";
 import {Button} from '@/components/ui/button'
 import Link from "next/link";
+import localfont from 'next/font/local';
+import {cn} from '@/lib/utils'
+import {Poppins} from 'next/font/google'
+import localFont from "next/font/local";
+
+const headingFont = localfont({
+    src: '../../public/fonts/font.woff2',
+})
+
+const textFont = Poppins({
+    subsets: ['latin'],
+    weight: [
+        '100',
+        '200',
+        '300',
+        '400',
+        '500',
+        '600',
+        '700',
+        '800',
+        '900',
+    ]
+})
 
 const MarketingPage = () => {
     return (
         <div className='flex items-center justify-center flex-col'>
-            <div className='flex items-center justify-center flex-col'>
+            <div className={cn('flex items-center justify-center flex-col', headingFont.className)}>
                 <div
                     className='mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase'>
                     <Medal className='h-6 w-6 mr-2'/>
@@ -19,7 +42,7 @@ const MarketingPage = () => {
                     work forward.
                 </div>
             </div>
-            <div className='text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto'>
+            <div className={cn('text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto', textFont.className)}>
                 Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the
                 way
                 your team works is unique - accomplish it all with Jirello.
