727be3adcffead84edb27e5385b2cc4429cd59c8
herohasan0 <kahraman.h@outlook.com>
2024-03-08 23:13:37 +0300

    update landing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,4 +1,6 @@
 import {Medal} from "lucide-react";
+import {Button} from '@/components/ui/button'
+import Link from "next/link";
 
 const MarketingPage = () => {
     return (
@@ -17,6 +19,16 @@ const MarketingPage = () => {
                     work forward.
                 </div>
             </div>
+            <div className='text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto'>
+                Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the
+                way
+                your team works is unique - accomplish it all with Jirello.
+            </div>
+            <Button className='mt-6' size='lg' asChild>
+                <Link href='/'>
+                    Get Jirello for free
+                </Link>
+            </Button>
         </div>
     )
 }
