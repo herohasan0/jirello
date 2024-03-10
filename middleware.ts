bb28b672e26284b1032e41c7264f36e71331b619
herohasan0 <kahraman.h@outlook.com>
2024-03-10 14:44:18 +0300

    update layout and middleware

---
+++ middleware.ts
@@ -0,0 +1,39 @@
+import {authMiddleware, redirectToSignIn} from "@clerk/nextjs";
+import {NextResponse} from "next/server";
+
+console.log('everused')
+
+export default authMiddleware({
+    publicRoutes: ['/'],
+    afterAuth(auth, req) {
+        console.log('everused2')
+
+        if (auth.userId && auth.isPublicRoute) {
+            console.log('auth', auth)
+            let path = '/select-org'
+
+            if (auth.orgId) {
+                path = `/organization/${auth.orgId}`
+            }
+
+            const orgSelection = new URL(path, req.url)
+            return NextResponse.redirect(orgSelection)
+        }
+
+        if (!auth.userId && !auth.isPublicRoute) {
+            return redirectToSignIn({returnBackUrl: req.url})
+        }
+
+        if (auth.userId && !auth.orgId && req.nextUrl.pathname !== '/select-org') {
+            const orgSelection = new URL('/select-org', req.url)
+            return NextResponse.redirect(orgSelection)
+        }
+    }
+});
+
+export const config = {
+    // Protects all routes, including api/trpc.
+    // See https://clerk.com/docs/references/nextjs/auth-middleware
+    // for more information about configuring your Middleware
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
